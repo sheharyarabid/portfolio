f9d898994a824de265fa6d387ce9a0675498744f
sheharyarabid <iamsheharyarabid@gmail.com>
2024-05-21 18:58:08 +0500

    Updated portfolio!

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+const logotext = "SHEHARYAR";
+const meta = {
+    title: "Sheharyar Abid",
+    description: "I’m Sheharyar Abid... From Islamadad, Pakistan. I have a passion for web development and love to create for web and mobile devices.",
+};
+
+const introdata = {
+    title: "Hi !  ... I’m Sheharyar Abid",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I’m a full stack developer, currently studying at Szabist. I have a passion for web development and love to create for web and mobile devices.",
+    your_img_url: "https://imgur.com/Ka0nX95.png",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I have a passion for developing cool and interactive web and mobile applications.",
+};
+const worktimeline = [{
+        jobtitle: "Full Stack (Intern)",
+        where: "Vizteck Solutions",
+        date: "2022",
+    },
+    {
+        jobtitle: "Freelance",
+        where: "Fiverr/Upwork",
+        date: "Present",
+    },
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 75,
+    },
+    {
+        name: "HTML/CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 60,
+    },
+    {
+        name: "NodeJS/ExpressJS",
+        value: 70,
+    },
+    {
+        name: "MongoDB / MySQL",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Figma to Webpages",
+        description: "Conversion of figma and photoshop designs to interactive webpages.",
+    },
+    {
+        title: "Web Development",
+        description: "MERN Stack, Bootstrap, Tailwind, JQuery, HTML/CSS & Javascript.",
+    },
+    {
+        title: "React Native",
+        description: "development of cross-platform applications using react native expo.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://imgur.com/HjMftEr.png",
+        description: "Keen Dashboard Clone using HTML/CSS Bootstrap and Javascript.",
+        link: "https://sheharyarabid.github.io/Keen-Dashboard",
+    },
+    
+    {
+        img: "https://imgur.com/pjm6NK8.png",
+        description: "Intel Homepage using HTML/CSS and Javascript.",
+        link: "#",
+    },
+    {
+        img: "https://imgur.com/hFjiTXZ.png",
+        description: "C++ based Vehcile Dealership program with OOP and DSA Concepts.",
+        link: "https://github.com/sheharyarabid/vehicle-dealership-Cplusplus",
+    },
+    {
+        img: "https://imgur.com/Te7J9qH.gif",
+        description: "",
+        link: "#",
+    },
+    {
+        img: "https://imgur.com/7q6fXeU.png",
+        description: "React Native, NodeJS and MongoDB based mobile app. MY FYP UnderDevelopment",
+        link: "https://sheharyarabid.github.io/bubble-game/",
+    },
+    {
+        img: "https://imgur.com/OBfxxbL.png",
+        description: "Basic Pizza Store using HTML/CSS.",
+        link: "#",
+    },
+
+    {
+        img: "https://imgur.com/Te7J9qH.gif",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://imgur.com/Te7J9qH.gif",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://imgur.com/GUyBuvl.png",
+        description: "E-Commerce Website for artists MERN stack.",
+        link: "https://imgur.com/188wPCY.png",
+    },
+    {
+        img: "https://imgur.com/LGxiNjn.png",
+        description: "NODEJS/EXPRESS JS based Learning management system.",
+        link: "#",
+    },
+    {
+        img: "https://imgur.com/Te7J9qH.gif",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://imgur.com/Te7J9qH.gif",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "iamsheharyarabid@gmail.com",
+    YOUR_FONE: "(+92)3345575157",
+    description: "Feel free to contact me to discuss any ideas and projects!. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/sheharyarabid",
+    facebook: "https://facebook.com",
+    linkedin: "Linkedin.com/in/sheharyarabid/",
+    twitter: "https://twitter.com/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
